a105b312a27ab09724b898cc49b527c17d5918bb
pashaRastaGL <zlodei2991@gmail.com>
2020-02-22 08:06:13 +0300

    верстка теста для сайта окон

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+
+
+
+var span_menu = $('');
+// var span_menu = $('');
+
+// var span_menu = $('');
+
+// var span_menu = $('');
+
+// var span_menu = $('');
+
+// var span_menu = $('');
+
+// var span_menu = $('');
+
+// var span_menu = $('');
+
+
+})
